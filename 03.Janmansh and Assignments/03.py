44e0d80583c0f6289d05ccc877f7dacefc6b9041
Poorani <tpoorani2002@gmail.com>
2024-02-14 13:43:30 +0530

    updated readme

---
+++ 03.Janmansh and Assignments/03.py
@@ -0,0 +1,10 @@
+# cook your dish here
+T = int(input())
+for i in range  (T):
+    X = int(input())
+    N= 3 # assignment 
+    time = X+N
+    if time <= 10 :
+        print("yes")
+    else:
+        print("No")
