c001984b10b955b4990a712f3000d3cf6e08e750
Poorani <tpoorani2002@gmail.com>
2024-03-12 09:37:40 +0530

    Day04

---
+++ Codechef/string/01.DNA storage/1.py
@@ -0,0 +1,19 @@
+t = int(input())
+
+while t > 0:
+    n = int(input())
+    s = input()
+    encoded_str=""
+    for i in range (0,n,2):
+        pair = s[i:i+2]
+        if pair == "00":
+            encoded_str +="A"
+        elif pair == "01":
+            encoded_str +="T"
+        elif pair == "10":
+            encoded_str +="C"
+            
+        elif pair == "11":
+            encoded_str +="G"
+    print(encoded_str)
+    t -= 1
