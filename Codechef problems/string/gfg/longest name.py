2ddf9060b97157ca94356b535bbab7b52c090196
POORANI TAMILVASAN <125793555+Poorani-27@users.noreply.github.com>
2024-06-28 22:05:19 +0530

    01

---
+++ Codechef problems/string/gfg/longest name.py
@@ -0,0 +1,14 @@
+'''Given a list of names, display the longest name. If there are multiple names of the longest size then return the first occurring name .
+
+Example1 :
+
+Input:
+n = 5
+names[] = { "Geek", "Geeks", "Geeksfor", "GeeksforGeek", "GeeksforGeeks" }
+Output: GeeksforGeeks
+Explanation: name "GeeksforGeeks" has maximum length among all names. '''
+
+
+n = 5
+names = { "Geek", "Geeks", "Geeksfor", "GeeksforGeek", "GeeksforGeeks" }
+print(max(names))
